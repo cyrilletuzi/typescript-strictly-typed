8a9cb23cee6a0320f40267b5bfc7f281e64321ec
Cyrille Tuzi <555867+cyrilletuzi@users.noreply.github.com>
2026-03-02 20:51:28 +0100

    enable node rules

--- src/cli.ts
+++ src/cli.ts
@@ -3,6 +3,7 @@ import { typescriptStrictlyTyped } from "./index.js";
 import { logError } from "./log-utils.js";
 
 /* Get the path where the command is invoked */
+// oxlint-disable-next-line promise/prefer-await-to-then -- Top-level await not supported for bin cli
 typescriptStrictlyTyped(cwd()).catch(() => {
   logError("Unknown error.\n");
 });
